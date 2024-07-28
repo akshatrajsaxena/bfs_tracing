44d81ccdf10d8b151a6e0dedf5253f46adb6c7e3
Akshat Raj Saxena <119042958+akshatrajsaxena@users.noreply.github.com>
2024-07-28 10:19:09 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import Game from './Game';
 import './App.css';
-
 function App() {
   return (
     <div className="App">
@@ -12,5 +11,4 @@ function App() {
     </div>
   );
 }
-
 export default App;
